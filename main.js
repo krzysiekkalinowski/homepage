28eb58975064bb0ccb968c6e4915a16c03a5d8c5
Krzyszot Kalinowski <krzykalinowski@gmail.com>
2023-12-22 14:59:43 +0100

    added working mobile menu

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const greet = (name, age) => `Witaj, nazywam się ${name} i mam ${age} lat`;
 
       const emptyArticle = document.querySelector('.article__content--js');
       emptyArticle.innerHTML = 'Ten tekst pojawił się dzięki JS';
+
+const hamburger = document.querySelector('.hamburger--js');
+hamburger.addEventListener('click', () => {
+      const navigation = document.querySelector('.top-navigation--js');
+      navigation.classList.toggle('top-navigation--open');
+})
+
