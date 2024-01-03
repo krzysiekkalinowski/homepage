c9fbef02c1ea648d69c322fb14e391951f5ffe20
Krzysztof Kalinowski <krzykalinowski@gmail.com>
2024-01-03 22:25:13 +0100

    added local timer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,19 @@ console.log(greetKrzysiek);
 
 const emptyArticle = document.querySelector('.article__content--js');
 emptyArticle.innerHTML = 'Ten tekst pojawił się dzięki JS';
+
+document.querySelectorAll('.timezone').forEach(function(element) {
+  const timezone = element.getAttribute('data-timezone');
+  if (timezone === 'local') {
+    const filledSpace = element.querySelector('.timezone__data');
+
+    // Funkcja aktualizująca czas
+    function updateLocalTime() {
+      const localDate = new Date();
+      filledSpace.innerHTML = localDate.toLocaleTimeString(); // Formatowanie czasu do czytelnej formy
+    }
+
+    updateLocalTime(); // Pierwsze ustawienie czasu
+    setInterval(updateLocalTime, 1000); // Aktualizacja czasu co 1 sekundę
+  }
+});
